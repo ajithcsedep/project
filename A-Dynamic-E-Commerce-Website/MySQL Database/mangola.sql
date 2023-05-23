2fb3f1fd75e5437c6c6538d97245fbeef4ce9916
purpleqube1 <134369948+purpleqube1@users.noreply.github.com>
2023-05-23 13:10:34 +0530

    Update mangola.sql

--- A-Dynamic-E-Commerce-Website/MySQL Database/mangola.sql
+++ A-Dynamic-E-Commerce-Website/MySQL Database/mangola.sql
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `222mangola`
+-- Database: `2228888mangola`
 --
 
 -- --------------------------------------------------------
